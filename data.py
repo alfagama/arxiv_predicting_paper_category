243d23dcf7d32f5462a3b0dbb39daf4b52d7d8c3
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2021-04-25 22:25:34 +0300

    20210425 - Base Code - v0.2

--- data.py
+++ data.py
@@ -6,4 +6,10 @@ def get_data():
                            header=0,
                            skiprows=0)
     pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    drop_columns = ['authors_parsed', 'comments', 'id', 'journal-ref', 'license', 'report-no', 'submitter']
+    data_set = data_set.drop(drop_columns, axis=1)
     print(data_set.head(5))
+
+    print(data_set['categories'].value_counts(ascending=True))
+    # data_set.categories.str.get_dummies(sep=' ').sum()
