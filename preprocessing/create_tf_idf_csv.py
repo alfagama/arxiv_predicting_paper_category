edf4d13fa87dcbc9f4642501c18db89d737032bc
alexi <alexia.fytili@gmail.com>
2021-05-09 18:51:46 +0300

    Tf idf changes 3.0

--- preprocessing/create_tf_idf_csv.py
+++ preprocessing/create_tf_idf_csv.py
@@ -12,14 +12,29 @@ def tf_idf_vectorizer():
     :return: -
     """
 
-    data = get_preprocessed_dataset()  # get preprcosessed dataset
+    data = get_preprocessed_dataset()  #.head(200)  # get preprcosessed dataset
     train_data, test_data = split_df_train_test(data)  # train test split
 
-    text_col = 'pre_abstract' #tochange
-
+    text_col = 'concatenation'  # tochange
+    print(train_data[text_col].values.astype('U'))
     tfidf = TfidfVectorizer(max_features=3000)  # min_df=10, out of memory exception when max_features=4000
-    train_data[text_col + 'tfidf'] = tfidf.fit_transform(train_data[text_col])
-    test_data[text_col + 'tfidf'] = tfidf.transform(test_data[text_col])
+    train_encodings = tfidf.fit_transform(train_data[text_col].values.astype('U'))
+    train_data[text_col + '_tfidf'] = list(train_encodings.toarray())  # Save the result in the new column
+
+    test_encodings = tfidf.transform(test_data[text_col].values.astype('U'))
+    test_data[text_col + '_tfidf'] = list(test_encodings.toarray())  # Save the result in the new column
 
     train_data.to_csv('../data/train_dataset_tfidf.csv')
     test_data.to_csv('../data/test_dataset_tfidf.csv')
+
+
+    # print tf idf scores (just for fun)
+    df = pd.DataFrame(train_encodings[0].T.todense(), index=tfidf.get_feature_names(), columns=["TF-IDF"])
+    df = df.sort_values('TF-IDF', ascending=False)
+    print(df.head(25))
+
+
+
+tf_idf_vectorizer()
+
+
