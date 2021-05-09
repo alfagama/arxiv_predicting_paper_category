b59e5e4614028e694bb2b97d924cc177bd6d1228
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2021-05-09 19:09:01 +0300

    20210509 - main changes for .csv files - v1.5.2

--- preprocessing/main_preprocessing.py
+++ preprocessing/main_preprocessing.py
@@ -17,21 +17,29 @@ def run_dataset_creation_process():
             raise
     # check for dataset from:
     # https://www.kaggle.com/Cornell-University/arxiv
-    # if os.path.isfile('../data/ArXiv_dataset.csv'):
-    #     # from preprocessing.filter_dataset_based_on_category
-    #     create_csv()
-    # if os.path.isfile('../data/dataset.csv'):
-    #     # from preprocessing.create_preprocessed_csv
-    #     preprocess()
+    if os.path.isfile('../data/dataset.csv'):
+        pass
+    elif os.path.isfile('../data/ArXiv_dataset.csv'):
+        # from preprocessing.filter_dataset_based_on_category
+        create_csv()
+
     if os.path.isfile('../data/preprocessed_conc_dataset.csv'):
+        pass
+    elif os.path.isfile('../data/dataset.csv'):
+        # from preprocessing.create_preprocessed_csv
+        preprocess()
+
+    if os.path.isfile('../data/category_columns_dataset.csv'):
+        pass
+    elif os.path.isfile('../data/preprocessed_conc_dataset.csv'):
         # from preprocessing.create_csv_with_categories_as_new_Columns
         create_csv_with_categories_as_new_Columns()
-    if os.path.isfile('../data/category_columns_dataset.csv'):
-        print("...")
+
+    if os.path.isfile('../data/test_dataset_tfidf.csv') & os.path.isfile('../data/train_dataset_tfidf.csv'):
+        pass
+    else:
         # from preprocessing.create_tf_idf_csv
-        # tf_idf_vectorizer()
-        # from preprocessing.tf_idf
-        # tf_idf()
+        tf_idf_vectorizer()
 
 
 run_dataset_creation_process()
