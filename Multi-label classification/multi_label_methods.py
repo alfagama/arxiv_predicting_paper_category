b06eb46e20fc73c4c64dd6e24a0cbe9346796254
alexi <alexia.fytili@gmail.com>
2021-05-14 00:24:03 +0300

    Multi-label Classification methods v1.0

---
+++ Multi-label classification/multi_label_methods.py
@@ -0,0 +1,84 @@
+from Imbalanced.imbalance_methods import *
+from dataset.dataset_methods_changed_by_me import *
+from skmultilearn.problem_transform import LabelPowerset
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import hamming_loss
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.naive_bayes import GaussianNB
+from skmultilearn.problem_transform import ClassifierChain
+from sklearn.linear_model import LogisticRegression
+
+
+def LabelPowerset(x_train, y_train, x_test, y_test):
+    lp_classifier = LabelPowerset(LogisticRegression())
+    lp_classifier.fit(x_train, y_train)
+    lp_predictions = lp_classifier.predict(x_test)
+
+    print("Accuracy = ", accuracy_score(y_test, lp_predictions))
+    print("F1 score = ", f1_score(y_test, lp_predictions, average="micro"))
+    print("Hamming loss = ", hamming_loss(y_test, lp_predictions))
+
+
+def BinaryRelevance(x_train, y_train, x_test, y_test):
+    br_classifier = BinaryRelevance(GaussianNB())
+    br_classifier.fit(x_train, y_train)
+    br_predictions = br_classifier.predict(x_test)
+
+    print("Accuracy = ", accuracy_score(y_test, br_predictions.toarray()))
+    print("F1 score = ", f1_score(y_test, br_predictions, average="micro"))
+    print("Hamming loss = ", hamming_loss(y_test, br_predictions))
+
+
+def ClassifierChains(x_train, y_train, x_test, y_test):
+    classifier = ClassifierChain(LogisticRegression())
+    classifier.fit(x_train, y_train)
+    predictions = classifier.predict(x_test)
+
+    print("Accuracy = ", accuracy_score(y_test, predictions.toarray()))
+    print("F1 score = ", f1_score(y_test, predictions, average="micro"))
+    print("Hamming loss = ", hamming_loss(y_test, predictions))
+
+
+if __name__ == '__main__':
+
+    # read data
+    df = pd.read_csv("../data/category_columns_dataset.csv",
+                     sep=',',
+                     header=0,
+                     skiprows=0).head(400)
+
+    # train test split
+    train, test = train_test_split(df, random_state=42, test_size=0.30, shuffle=True)
+
+
+    # Tf idf vectorizer
+    # ----------------------------------------------------------
+    train_text = train['concatenation'].values.astype('U')
+    test_text = test['concatenation'].values.astype('U')
+
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1, 3), norm='l2',
+                                 max_features=10000)
+    vectorizer.fit(train_text)
+    x_train = vectorizer.transform(train_text)  # .toarray()
+    x_test = vectorizer.transform(test_text)  # .toarray()
+    # ----------------------------------------------------------
+
+    y_train = train.drop(labels=['abstract', 'title', 'concatenation', 'categories', 'Unnamed: 0', 'Unnamed: 0.1'],
+                         axis=1)
+    y_test = test.drop(labels=['abstract', 'title', 'concatenation', 'categories', 'Unnamed: 0', 'Unnamed: 0.1'],
+                       axis=1)
+
+    print('tf_idf scores: \n', sorted(list(zip(vectorizer.get_feature_names(),
+                                               x_train.sum(0).getA1())),
+                                      key=lambda x: x[1], reverse=True)[:40])
+
+
+
+    LabelPowerset(x_train, y_train, x_test, y_test)
+    BinaryRelevance(x_train, y_train, x_test, y_test)
+    ClassifierChains(x_train, y_train, x_test, y_test)
+
