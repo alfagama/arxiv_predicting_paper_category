b66dd41d980e897b3174da84b9949cd2cf846194
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2021-05-15 23:14:37 +0300

    20210515 - Labels from MultiLabelBinarizer - v1.8

--- preprocessing/create_csv_with_categories_as_new_Columns.py
+++ preprocessing/create_csv_with_categories_as_new_Columns.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
 
 # Options for pandas -----
 pd.set_option('display.max_columns', None)
@@ -42,3 +43,32 @@ def create_csv_with_categories_as_new_Columns():
     # export csv
     data_set.to_csv('../data/category_columns_dataset.csv')
     print(data_set.head(5))
+
+
+def multilabelBinarizer_method():
+    """
+        creates category_columns_dataset.csv from preprocessed_dataset.csv
+        :return: -
+        """
+    # reads data/preprocessed_dataset.csv
+    data_set = pd.read_csv("../data/preprocessed_conc_dataset.csv",
+                           sep=',',
+                           header=0,
+                           skiprows=0)
+
+    print(data_set.head(5))
+
+    mlb = MultiLabelBinarizer()
+    labels = mlb.fit_transform(data_set.categories)
+    # for i in labels:
+    #     print(i)
+    # print(labels)
+
+    # concatenate with the abstracts
+    df = pd.concat([data_set[['abstract', 'title']], pd.DataFrame(labels)], axis=1)
+    df.columns = ['abstract', 'title'] + list(mlb.classes_)
+    # print(df.head(40))
+
+    # export csv
+    data_set.to_csv('../data/category_columns_dataset_with_list_from_multilabelBinarizer_method.csv')
+    print(data_set.head(40))
