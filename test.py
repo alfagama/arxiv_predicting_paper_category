2e06ff3e8b4dc9451e4ea0b183477a87c32e8549
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2021-05-28 22:00:52 +0300

    20210528 - Random Undersampling - v1.9

---
+++ test.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+# Options for pandas -----
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+
+def aaaa():
+    """
+    creates category_columns_dataset.csv from preprocessed_dataset.csv
+    :return: -
+    """
+    # reads data/preprocessed_dataset.csv
+    data_set = pd.read_csv("data/generated_notshuffled_textcategories_nostopwords.csv",
+                           sep=',',
+                           header=0,
+                           skiprows=0)
+
+    print(data_set.head(5))
+
+    col_one_list = data_set['categories'].tolist()
+    unique_list = []
+    category_Names = []
+
+    for x in col_one_list:
+        if x not in unique_list:
+            unique_list.append(x)
+
+    for categories in unique_list:
+        split_categories = categories.split(" ")
+        for category in split_categories:
+            if category not in category_Names:
+                category_Names.append(category)
+
+    print('Category names are:')
+    print(category_Names)
+
+    for x in category_Names:
+        contains = data_set['categories'].str.contains(x)
+        data_set[x] = contains.astype(int)
+
+    # export csv
+    data_set.to_csv('data/generated_notshuffled_20categories_nostopwords.csv')
+    print(data_set.head(5))
+
+
+aaaa()
